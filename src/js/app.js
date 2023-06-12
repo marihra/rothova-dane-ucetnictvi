cd0faea67003494204a949fd8358ba1af0867e9c
mhrabovska <mhrabovska@vccp.com>
2023-06-12 10:27:47 +0200

    Add testimonials

--- src/js/app.js
+++ src/js/app.js
@@ -23,3 +23,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
 });
+
+// Replace the normal space after a single letter word with a non-breaking space
+function fixSingleLetterWords(text) {
+    const prepositions = ['a', 'i', 'k', 'o', 's', 'u', 'v', 'z'];
+    const regex = new RegExp(`\\b(${prepositions.join('|')})\\s`, 'gm');
+    const fixedText = text.replace(regex, '$1&nbsp;');
+  
+    return fixedText;
+  }
+
+// Apply the function to <p> elements
+const paragraphs = document.getElementsByTagName('p');
+for (let i = 0; i < paragraphs.length; i++) {
+  const paragraph = paragraphs[i];
+  paragraph.innerHTML = fixSingleLetterWords(paragraph.innerHTML);
+}
